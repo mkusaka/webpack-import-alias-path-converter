6aac0a3a014061d27505d47c251a57d960d2cf48
mkusaka <hinoshita1992@gmail.com>
2020-08-30 02:26:01 +0900

    chore: minimal jscodeshift example

---
+++ transformer.js
@@ -0,0 +1,9 @@
+/**
+ * @type {import("jscodeshift").Transform}
+ */
+module.exports = function (fileInfo, api) {
+  return api.jscodeshift(fileInfo.source)
+    .findVariableDeclarators('foo')
+    .renameTo('bar')
+    .toSource();
+}
