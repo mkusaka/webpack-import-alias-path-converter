7d2556c3ca6767e2cb94d984cee45dec7b1f8d39
mkusaka <hinoshita1992@gmail.com>
2020-08-30 15:32:43 +0900

    refactor: remove lodash dependencies

--- transformer.js
+++ transformer.js
@@ -1,14 +1,12 @@
 const j = require("jscodeshift");
-const { resolve, dirname, delimiter } = require("path");
-const { invert } = require("lodash-es");
+const { resolve, dirname } = require("path");
 
-const alias = {
+const pathAlias = {
   subdir: resolve(__dirname, "example/subdirb"),
   src: resolve(__dirname, "example"),
 };
 
-const invertedAlias = invert(alias);
-const aliasedKeys = Object.keys(alias);
+const aliasedKeys = Object.keys(pathAlias);
 
 /**
  * @type {import("jscodeshift").Transform}
@@ -26,8 +24,8 @@ module.exports = function (fileInfo, api) {
       const sourceDirectoryPath = resolve(dirname(fileInfo.path));
       const dir = resolve(sourceDirectoryPath, importName);
       let aliasedPath = importName;
-      for (const alias of Object.entries(invertedAlias)) {
-        const [path, aliased] = alias;
+      for (const alias of Object.entries(pathAlias)) {
+        const [aliased, path] = alias;
         if (dir.match(new RegExp(`^${path}`))) {
           aliasedPath = dir.replace(new RegExp(`^${path}`), aliased);
           break;
